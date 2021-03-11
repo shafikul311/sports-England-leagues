418a1bfa38c50c14089a51e54f6cd9cc5641572d
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-03-11 19:44:50 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from "./component/Home/Home";
 import TeamDetails from "./component/TeamDetails/TeamDetails";
 import NoMatch from "./component/NoMatch/NoMatch";
